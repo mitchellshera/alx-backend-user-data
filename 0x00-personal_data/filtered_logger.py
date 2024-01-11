8f1a67740cbf85e9250632c3df1e228cee602e3c
mitchellshera <mitchellwachera@gmail.com>
2024-01-11 23:10:57 +0300

    list

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,7 +12,7 @@ import logging
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
-def filter_datum(fields: List[str], redaction: str,
+def filter_datum(fields: list[str], redaction: str,
                  message: str, separator: str) -> str:
     """
     Obscures specified fields in a log message.
