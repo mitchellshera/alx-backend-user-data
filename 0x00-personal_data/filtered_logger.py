5d6f84853c88a459ab432daf59656d82d52d5232
mitchellshera <mitchellwachera@gmail.com>
2024-01-11 22:48:15 +0300

    task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,22 +4,61 @@ This module provides a function for obfuscating
 log messages by replacing specified field values.
 """
 
-import logging
-from typing import List
 import re
+from typing import List
+import logging
+
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+FIELDS = ('name', 'email', 'phone', 'ssn', 'password',
+          'ip', 'last_login', 'user_agent')
 
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class.
+
+    This class extends the logging.Formatter class
+    and provides a custom log formatting
+    with redacted sensitive information in log records.
+
+    Attributes:
+        redaction (str): The string used for redacting sensitive information.
+        format_str (str): The log record format.
+        separator (str): The character separating fields in log messages.
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize RedactingFormatter"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format a log message"""
+        message = super(RedactingFormatter, self).format(record)
+        redacted = filter_datum(self.fields, self.REDACTION,
+                                message, self.SEPARATOR)
+        return redacted
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
-    Return an obfuscated log message
+    Obfuscates specified fields in a log message.
+
     Args:
-        fields (list): list of strings indicating fields to obfuscate
-        redaction (str): what the field will be obfuscated to
-        message (str): the log line to obfuscate
-        separator (str): the character separating the fields
+        fields (List[str]): A list of strings
+        representing fields to obfuscate in the log message.
+        redaction (str): A string representing
+        the value by which the field will be obfuscated.
+        message (str): A string representing the log line.
+        separator (str): A string representing the
+        character separating all fields in the log line.
+
+    Returns:
+        str: The obfuscated log message.
     """
     for field in fields:
         message = re.sub(field + '=.*?' + separator,
@@ -27,48 +66,23 @@ def filter_datum(fields: List[str], redaction: str,
     return message
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """
-        redact the message of LogRecord instance
-        Args:
-        record (logging.LogRecord): LogRecord instance containing message
-        Return:
-            formatted string
-        """
-        message = super(RedactingFormatter, self).format(record)
-        redacted = filter_datum(self.fields, self.REDACTION,
-                                message, self.SEPARATOR)
-        return redacted
-
-
-def get_logger() -> logging.Logger:
+def get_logger():
     """
-    Return a logging.Logger object
+    Get a logger named "user_data" with a StreamHandler and RedactingFormatter.
+
+    Returns:
+        logging.Logger: The configured logger.
     """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
-    logger.propagate = False
 
-    handler = logging.StreamHandler()
-
+    # Create a StreamHandler
+    stream_handler = logging.StreamHandler()
     formatter = RedactingFormatter(PII_FIELDS)
 
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
     return logger
-
 
 if __name__ == "__main__":
     pass
