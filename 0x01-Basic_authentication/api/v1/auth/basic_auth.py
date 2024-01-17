a3a9e31bf6decfb10d4a11e4325f4849e496a857
mitchellshera <mitchellwachera@gmail.com>
2024-01-17 22:03:31 +0300

    Done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -77,7 +77,6 @@ class BasicAuth(Auth):
             return None
         except Exception:
             return None
-        
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
