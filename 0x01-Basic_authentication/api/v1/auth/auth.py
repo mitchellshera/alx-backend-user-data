fd16860712aecee2cd00e99b9aa52b0ba140dabc
mitchellshera <mitchellwachera@gmail.com>
2024-01-17 17:19:22 +0300

    py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@ auth module
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     '''Auth class'''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
