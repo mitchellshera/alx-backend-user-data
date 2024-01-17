914222819f813d5b361e1dfaace5667b63e66502
mitchellshera <mitchellwachera@gmail.com>
2024-01-17 16:06:54 +0300

    route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,17 +4,17 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized_endpoint() -> str:
-    """ GET /api/v1/unauthorized
+    """ GET /unauthorized
     Raises:
       - 401 error using abort
     """
     abort(401)
 
-@app_views.route('/api/v1/forbidden', methods=['GET'], strict_slashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden_endpoint() -> str:
-    """ GET /api/v1/forbidden
+    """ GET /forbidden
     Return:
       - JSON response for forbidden request
     """
