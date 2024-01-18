810778da5c315ad351f55fae6a5ff67dd104cf77
mitchellshera <mitchellwachera@gmail.com>
2024-01-18 16:07:39 +0300

    0

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -28,10 +28,8 @@ def view_one_user(user_id: str = None) -> str:
     if user_id is None:
         abort(404)
 
-     # Check if <user_id> is equal to 'me' and request.current_user is None
     if user_id == 'me' and request.current_user is None:
         abort(404)
-    # Check if <user_id> is equal to 'me' and request.current_user is not None
     if user_id == 'me' and request.current_user is not None:
         return jsonify(request.current_user.to_json())
 
