4c35c29d7ad47cfff8c8c89977d848d101ce44a8
mitchellshera <mitchellwachera@gmail.com>
2024-01-24 21:43:28 +0300

    0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''module for user.py'''
 
-from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
