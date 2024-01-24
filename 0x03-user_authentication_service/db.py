6838a232b8ec2a0f69e5b3fd319ec16913dd3f06
mitchellshera <mitchellwachera@gmail.com>
2024-01-24 21:54:04 +0300

    db

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''module for db.py'''
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import Base, User
+
+
+class DB:
+    def __init__(self) -> None:
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
