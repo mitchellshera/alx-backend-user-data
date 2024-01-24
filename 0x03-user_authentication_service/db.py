ffd3dd853f92139db99472b545b0679456c22904
mitchellshera <mitchellwachera@gmail.com>
2024-01-25 00:17:10 +0300

    2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,11 +5,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 class DB:
+    '''DB class'''
     def __init__(self) -> None:
+        '''constructor'''
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -17,18 +20,21 @@ class DB:
 
     @property
     def _session(self) -> Session:
+        '''creates a session if not created'''
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
+        '''takes mandatory email and hashed_password fields and returns a User'''
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
+        '''takes in arbitrary keyword arguments and returns the first row'''
         user = self._session.query(User)
         for k, v in kwargs.items():
             if k not in User.__dict__:
