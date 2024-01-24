2219d321f495a69342412fb3e466a1a3ac021681
mitchellshera <mitchellwachera@gmail.com>
2024-01-25 00:22:18 +0300

    3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -27,7 +27,8 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        '''takes mandatory email and hashed_password fields and returns a User'''
+        '''takes mandatory email and hashed_password
+        fields and returns a User'''
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
@@ -36,10 +37,32 @@ class DB:
     def find_user_by(self, **kwargs) -> User:
         '''takes in arbitrary keyword arguments and returns the first row'''
         user = self._session.query(User)
-        for k, v in kwargs.items():
-            if k not in User.__dict__:
+        for key, value in kwargs.items():
+            if key not in User.__dict__:
                 raise InvalidRequestError
             for usr in user:
-                if getattr(usr, k) == v:
+                if getattr(usr, key) == value:
                     return usr
         raise NoResultFound
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Update a user's attributes based on the provided arguments.
+
+        Args:
+            user_id (int): The ID of the user to update.
+            kwargs: Arbitrary keyword arguments representing user attributes.
+
+        Raises:
+            ValueError: If an argument that does not
+            correspond to a user attribute is passed.
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if key not in User.__dict__:
+                    raise ValueError(f"Invalid user attribute: {key}")
+                setattr(user, key, value)
+            self._session.commit()
+        except NoResultFound:
+            raise ValueError(f"No user found with ID: {user_id}")
