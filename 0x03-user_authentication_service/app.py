12fbe57f1d4027b3f2e9e7d68309ac852bca27bb
mitchellshera <mitchellwachera@gmail.com>
2024-01-25 13:32:46 +0300

    19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -120,7 +120,7 @@ def get_reset_password_token():
         'reset_token': reset_token
     }), 200
 
-@app.route('/reset_password', methods=['PUT'])
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     '''update_password function to respond to the PUT /reset_password route.'''
     try:
